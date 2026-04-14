217f28211b6aa2a2f232d3745a8e5a1f96e74369
jeeva <jeevajeevajo123@gmail.com>
2026-04-14 12:41:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from model import predict_waiting_time
 
 patients = int(input("Enter patients ahead: "))
-avg_time = int(input("Enter avg time: "))
+avg_time = int(input("Enter avg time per patient: "))
 
 waiting_time = predict_waiting_time(patients, avg_time)
 
 print(f"Estimated waiting time: {waiting_time} minutes")
 
-if waiting_time < 20:
+if waiting_time <= 30:
     print("Best time to visit ✅")
+elif waiting_time <= 60:
+    print("Moderate waiting time ⚠️")
 else:
     print("Try another slot ❌")
